aea590efde3eab5d01b55423a8c2f65ed1f75c63
cgp <chenguopeng@linctex.com>
2024-11-04 11:26:07 +0800

    个人信息 + -

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,148 @@
-import Image from "next/image";
+'use client'
+import { useState } from "react";
+import TopImg from './assets/img/TopImg.png'
+import './style.css';
 
-export default function Home() {
+type user = 
+  {
+    name:'xx',
+    age:1,
+    score:1,
+  }
+
+function NameButton({onClick}) {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      <div className="button" onClick={onClick}>信息</div>
     </div>
   );
 }
+function AddButton({onClick}) {
+  return (
+    <div>
+      <div className="button" onClick={onClick}>+1</div>
+    </div>
+  );
+}
+function ReduceButton({onClick}) {
+  return (
+    <div>
+      <div className="button" onClick={onClick}>-1</div>
+    </div>
+  );
+}
+
+export default function Home(){
+  const [userList,setUserList]=useState([] as Array<user>);
+
+  async function handleClick (){
+    let res = await fetch('http://localhost:3000/users/user',{
+      method:"GET",
+    })
+    res = await res.json();
+     let userList = res.map(i=>{
+      console.log('i',i);
+      return {
+        name:i.name,
+        age:i.age,
+        score:i.score,
+      }
+    })
+
+    setUserList(
+      userList
+    )
+  }
+  async function handleClickAdd (){
+    let res = await fetch('http://localhost:3000/users/user/add',{
+      method:"GET",
+    })
+    res = await res.json();
+     let userList = res.map(i=>{
+      console.log('i',i);
+      return {
+        name:i.name,
+        age:i.age,
+        score:i.score,
+      }
+    })
+
+    setUserList(
+      userList
+    )
+  }
+  async function handleClickReduce (){
+    let res = await fetch('http://localhost:3000/users/user/reduce',{
+      method:"GET",
+    })
+    res = await res.json();
+     let userList = res.map(i=>{
+      console.log('i',i);
+      return {
+        name:i.name,
+        age:i.age,
+        score:i.score,
+      }
+    })
+
+    setUserList(
+      userList
+    )
+  }
+  
+  return(
+    <div>
+      {
+        userList.map(i=>{
+          return (
+            <div>
+              <div>姓名：{i.name}</div>
+              <div>年龄：{i.age}</div>
+              <div>分数：{i.score}</div>
+            </div>
+          )
+        })
+      } 
+      <NameButton onClick={handleClick}></NameButton>
+      <AddButton onClick={handleClickAdd}></AddButton>
+      <ReduceButton onClick={handleClickReduce}></ReduceButton>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+function AdButton({onClick}) {
+  return (
+    <div>
+      <div onClick={onClick} className="button">count+</div>
+    </div>
+  )
+}
+function SmallButton({onClick}) {
+  return (
+    <div>
+      <div onClick={onClick} className="button">count-</div>
+    </div>
+  )
+}
+// export default function New(){
+//   const [count1,setCount1]=useState(100)
+//   function handleClick1(){
+//     setCount1(count1+1)
+//   }
+//   function handleClick2(){
+//     setCount1(count1-1)
+//   }
+//   return(
+//     <div>
+//       <div>{count1}</div>
+//       <AdButton onClick={handleClick1}></AdButton>
+//       <SmallButton onClick={handleClick2}></SmallButton>
+//     </div>
+//   )
+// }
